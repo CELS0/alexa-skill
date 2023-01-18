a8001a76aff8913ae60fa046035e62e3fecae996
Celso Júnior <webcode109@gmail.com>
2023-01-18 20:16:02 -0300

    feat

---
+++ index.js
@@ -0,0 +1,40 @@
+const Alexa = require("ask-sdk-core");
+
+const LaunchRequestHandler = {
+  canHandle(handlerInput) {
+    return (
+      Alexa.getRequestType(handlerInput.requestEnvelope) === "LaunchRequest"
+    );
+  },
+  handle(handlerInput) {
+    const speechText = "Deu bom!";
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .withSimpleCard(
+        "Welcome to your SDK weather skill. Ask me the weather!",
+        speechText
+      )
+      .getResponse();
+  },
+};
+
+let skill;
+
+exports.handler = async function (event, context) {
+  console.log(`REQUEST++++${JSON.stringify(event)}`);
+  if (!skill) {
+    skill = Alexa.SkillBuilders.custom()
+      .addRequestHandlers(
+        LaunchRequestHandler,
+      )
+      .addErrorHandlers(ErrorHandler)
+      .create();
+  }
+
+  const response = await skill.invoke(event, context);
+  console.log(`RESPONSE++++${JSON.stringify(response)}`);
+
+  return response;
+};
